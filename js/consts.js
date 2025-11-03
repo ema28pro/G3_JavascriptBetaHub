1f770651c7c042eb6c96a50c188f3854ad3a1186
Emanuel Lopez <ema2805pro@gmail.com>
2025-11-03 18:33:11 -0500

    Primera Implementacion de Swal y Toastify con boton de agregar proviccional

--- js/consts.js
+++ js/consts.js
@@ -1,4 +1,4 @@
 const auth = {
-    emailUser: "bauti333@gmail.com",
+    emailUser : "bauti333@gmail.com",
     passwordUser : "12345"
 };
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const buscador = document.getElementById("buscador");
 const listaCategoria = document.getElementById("category");
 const barraBusqueda = document.querySelector(".search-container");
 
-const categoriasBtn = categorylist.map(categoria => 
+const categoriasBtn = categorylist.map(categoria =>
     `<li><button class="category-btn" onclick="seleccionarCategoria('${categoria}')">${categoria}</button></li>`
 );
 
@@ -60,6 +60,7 @@ function mostrarCards(productos) {
             <p>${producto.short_description}</p>
             <span>Precio: ${producto.price.toLocaleString('es-CO', { style: 'currency', currency: 'COP' })}</span>
             <br>
+            <button onclick="addToCar('${producto.name}')">Carrito</button>
             <a href="detalles.html?prod=${producto.id}">Ver más</a>
         </div>`
     );
@@ -106,3 +107,21 @@ function quitarBoton() {
 
 buscador.addEventListener("input", realizarBusqueda); // Es ineficiente para muchos datos
 // buscador.addEventListener("input", mostrarBoton)
+
+function addToCar(productName) {
+    Swal.fire({
+        title: '¡Alerta!',
+        text: `¿Estas seguro que quieres añadir el producto ${productName}?`,
+        // icon: 'success',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No',
+        showCancelButton: true,
+        showCloseButtton: true
+    }).then(result => {
+        if (result.isConfirmed) {
+            Toastify({
+                text: `${productName} Agregado`
+            }).showToast()
+        };
+    });
+};
